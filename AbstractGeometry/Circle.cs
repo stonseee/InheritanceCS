59ece011a2076cc9abaee034576da4c644ae14a4
stonseee <stonseee@STONSEEE>
2024-10-20 03:51:49 +0300

    Homework DONE

--- AbstractGeometry/Circle.cs
+++ AbstractGeometry/Circle.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace AbstractGeometry
 {
-	internal class Circle:Shape
+	internal class Circle:Shape, IHaveDiameter
 	{
 		double radius;
 		public double Radius
@@ -24,6 +24,10 @@ namespace AbstractGeometry
 		{
 			Radius = radius;
 		}
+		public double GetDiameter()
+		{
+			return 2 * Radius;
+		}
 		public override double GetArea() => Math.PI * Math.Pow(Radius, 2);
 		public override double GetPerimeter() => 2 * Math.PI * Radius;
 		public override void Draw(PaintEventArgs e)
@@ -31,6 +35,11 @@ namespace AbstractGeometry
 			Pen pen = new Pen(Color, LineWidth);
 			//e.Graphics.DrawEllipse(pen, new RectangleF((int)StartX, (int)StartY, (int)(2 * Radius), (int)(2 * Radius)));
 			e.Graphics.DrawEllipse(pen, StartX, StartY, 2 * (float)Radius, 2 * (float)Radius);
+			DrawDiameter(e);
+		}
+		public void DrawDiameter(System.Windows.Forms.PaintEventArgs e)
+		{
+			e.Graphics.DrawLine(new Pen(Color, 1), StartX + (int)Radius, StartY, StartX + (int)Radius, StartY + (int)GetDiameter());
 		}
 		public override void Info(PaintEventArgs e)
 		{
